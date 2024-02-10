43e630cf30ccfb8708d1852271b39d4569d9b9c4
Solomentsevaolya <152183629+Solomentsevaolya@users.noreply.github.com>
2024-02-10 16:32:46 +1000

    Update laba1.c
    erererererer

--- laba1.c
+++ laba1.c
@@ -3,39 +3,50 @@
 
 int main()
 {
+    int i;
+    do {
     
-    
-    double a, b, c, d, x1, x2;
-    a = 5.1;
-    b = 1;
-    c = -2.2;
-
+        double a, b, c, d, x1, x2;
+        a = 5.1;
+        b = 1;
+        c = -2.2;
         
-    printf ("Введите a:");
-    scanf("%lf", &a);
-    printf ("Введите b:");
-    scanf("%lf", &b);
-    printf ("Введите c:");
-    scanf("%lf", &c);
+       
+        
+        printf ("Введите a:");
+        scanf("%lf", &a);
+        printf ("Введите b:");
+        scanf("%lf", &b);
+        printf ("Введите c:");
+        scanf("%lf", &c);
+        
+        
+        d = b*b - 4*a*c;
+        
+        printf("d=%lf\n",d);
+        
+        if (d>0){
+            x1 = (-b + sqrt(d)/(2*a));
+            x2 = (-b - sqrt(d)/(2*a));
+        
+            printf("x1=%lf x2=%lf\n", x1, x2);
+        }
+        
+        if (d==0) {
+            x1 = (-b)/(2*a);
+            printf("x1=%lf\n",x1);
+        }
+        
+        if (d<0){
+            printf("корней нет\n");
+        }
+        
     
-    d = b*b - 4*a*c;
-    
-    printf("d=%lf\n",d);
-    
-    if (d>0){
-        x1 = (-b + sqrt(d)/(2*a));
-        x2 = (-b - sqrt(d)/(2*a));
-    
-    printf("x1=%lf x2=%lf", x1, x2);
+        printf("Продолжить? 1-да 2-нет");
+        scanf("%d", &i);
+        
     }
-    
-    if (d==0) {
-        x1 = (-b)/(2*a);
-        printf("x1=%lf",x1);
-    }
-    
-    if (d<0){
-        printf("корней нет");
-    }
+    while (i == 1);
+   
     return 0;
 }
